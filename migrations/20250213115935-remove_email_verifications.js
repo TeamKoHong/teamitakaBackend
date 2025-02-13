a2d1e69e1b19fa01fb7f1942f5fb5b08d9b651b5
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-13 21:10:10 +0900

    🔨 Fix: Remove unused variables
    Fix: Remove unused variables

--- migrations/20250213115935-remove_email_verifications.js
+++ migrations/20250213115935-remove_email_verifications.js
@@ -2,21 +2,40 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up (queryInterface) {
+    // email_verifications 테이블 삭제
+    await queryInterface.dropTable('email_verifications');
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    // 복구: email_verifications 테이블 다시 생성
+    await queryInterface.createTable('email_verifications', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      otp: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      expiresAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      }
+    });
   }
 };
