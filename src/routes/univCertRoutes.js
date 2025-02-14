7c42dd5949eeed7d5c185f4e026177b2beecded2
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-15 00:48:45 +0900

    ✨ New Feature: UnivCert email verification feature
    New Feature: UnivCert email verification feature

--- src/routes/univCertRoutes.js
+++ src/routes/univCertRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const univCertController = require("../controllers/univCertController");
 
-router.post("/send-otp", univCertController.sendVerificationCode);
-router.post("/verify-otp", univCertController.verifyCode); // ✅ verifyCode 라우트 추가
+router.post("/send-otp", univCertController.sendOtp);
+router.post("/verify-otp", univCertController.verifyOtp); // ✅ verifyCode 라우트 추가
 
 module.exports = router;
