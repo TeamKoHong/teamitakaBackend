fd6f05225f723fa3ee1238f62f6b56000272be35
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-14 13:12:14 +0900

    ✨ New Model: VerifiedEmail
    New Model: VerifiedEmail

---
+++ src/migrations/20250214041108-create_verified_emails.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("VerifiedEmails", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      certified_date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      expires_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("VerifiedEmails");
+  },
+};
