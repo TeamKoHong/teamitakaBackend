a31861c6afa2274f9a6eb3a261ee224ec6011cb0
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-19 21:39:58 +0900

    🔧 Update prod DB settings for IAM auth&Auth Proxy
    Update prod DB settings for IAM auth&Auth Proxy

--- src/config/config.js
+++ src/config/config.js
@@ -10,10 +10,10 @@ module.exports = {
     logging: false,
   },
   production: {
-    // Cloud SQL Auth Proxy를 사용하는 경우, 데이터베이스는 항상 로컬(127.0.0.1)을 통해 연결됩니다.
-    // IAM 인증 사용자로 설정되어 있으므로, 패스워드는 빈 문자열("")이어야 합니다.
+    // Cloud SQL Auth Proxy가 127.0.0.1:3306에서 실행되므로 호스트를 127.0.0.1로 고정합니다.
+    // IAM 인증 사용자이므로 비밀번호는 빈 문자열("")입니다.
     username: process.env.GCP_DB_USER || "iam_user",
-    password: "", // IAM 인증 사용자이므로 비밀번호는 사용하지 않습니다.
+    password: "",
     database: process.env.GCP_DB_NAME || "teamitaka_database",
     host: "127.0.0.1",
     port: 3306,
