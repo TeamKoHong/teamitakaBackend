6eca56c256c8f9f7c03688867ae558534d6bd908
yewon <142921713+yewon4284@users.noreply.github.com>
2025-02-20 00:43:14 +0900

    되나

--- src/models/index.js
+++ src/models/index.js
@@ -5,31 +5,31 @@ const Sequelize = require("sequelize");
 
 const db = {};
 db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-if (sequelize) {
-  db.sequelize = sequelize;
-  db.Keyword = require("./Keyword")(sequelize, Sequelize.DataTypes);
-  db.Notification = require("./Notification")(sequelize, Sequelize.DataTypes);
-  db.Project = require("./Project")(sequelize, Sequelize.DataTypes);
-  db.Recruitment = require("./Recruitment")(sequelize, Sequelize.DataTypes);
-  db.Review = require("./Review")(sequelize, Sequelize.DataTypes);
-  db.Search = require("./Search")(sequelize, Sequelize.DataTypes);
-  db.User = require("./User")(sequelize, Sequelize.DataTypes);
-  db.VerifiedEmail = require("./VerifiedEmail")(sequelize, Sequelize.DataTypes);
-  db.Admin = require("./Admin")(sequelize, Sequelize.DataTypes);
-  db.University = require("./University")(sequelize, Sequelize.DataTypes);
-  db.College = require("./College")(sequelize, Sequelize.DataTypes);
-  db.Department = require("./Department")(sequelize, Sequelize.DataTypes);
-  db.Application = require("./Application")(sequelize, Sequelize.DataTypes);
-  
-  Object.values(db).forEach((model) => {
-    if (model && model.associate) {
-      console.log('Associating model: ${model.name}');
-      model.associate(db);
-    }
-  });
-} else {
-  console.log("🚨 Sequelize is not initialized. Skipping model setup.");
-}
+// 모델 추가
+db.Keyword = require("./Keyword")(sequelize, Sequelize.DataTypes);
+db.Notification = require("./Notification")(sequelize, Sequelize.DataTypes);
+db.Project = require("./Project")(sequelize, Sequelize.DataTypes);
+db.Recruitment = require("./Recruitment")(sequelize, Sequelize.DataTypes);
+db.Review = require("./Review")(sequelize, Sequelize.DataTypes);
+db.Search = require("./Search")(sequelize, Sequelize.DataTypes);
+db.User = require("./User")(sequelize, Sequelize.DataTypes);
+db.VerifiedEmail = require("./VerifiedEmail")(sequelize, Sequelize.DataTypes);
+db.Admin = require("./Admin")(sequelize, Sequelize.DataTypes);
+db.University = require("./University")(sequelize, Sequelize.DataTypes);
+db.College = require("./College")(sequelize, Sequelize.DataTypes);
+db.Department = require("./Department")(sequelize, Sequelize.DataTypes);
+db.Application = require("./Application")(sequelize, Sequelize.DataTypes);
+db.Hashtag = require("./Hashtag")(sequelize, Sequelize.DataTypes); // ✅ 추가
+db.RecruitmentHashtag = require("./RecruitmentHashtag")(sequelize, Sequelize.DataTypes); // ✅ 추가
+
+// 모델 간 관계 설정
+Object.values(db).forEach((model) => {
+  if (model && model.associate) {
+    console.log(`Associating model: ${model.name}`);
+    model.associate(db);
+  }
+});
 
 module.exports = db;
