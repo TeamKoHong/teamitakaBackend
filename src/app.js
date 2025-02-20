2fe9e2aa829ce1c5f922e6cdd46ad5aa832e989a
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-20 13:13:45 +0900

    🔨 Modified: app.js & Recruitment.js
    Modified: app.js & Recruitment.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser"); // ✅ 추가
 
 const adminRoutes = require("./routes/adminRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -12,13 +11,11 @@ const userRoutes = require("./routes/userRoutes"); // ← 새로 추가
 const recruitmentRoutes = require("./routes/recruitmentRoutes");// ← 새로 추가
 const commentRoutes = require("./routes/commentRoutes");// ← 새로 추가
 const likeRoutes = require("./routes/likeRoutes");// ← 새로 추가
-const applicationRoutes = require("./routes/applicationRoutes");//지원
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cookieParser()); // ✅ 추가 (쿠키 기반 조회수 기능 위해 필요)
 
 // 이미 등록된 라우트
 app.use("/api/admin", adminRoutes);
@@ -35,10 +32,6 @@ app.use("/api", [
   commentRoutes,
   likeRoutes,
 ]);
-
-app.use("/api/user", applicationRoutes);
-app.use("/api/user", scrapRoutes);
-
 
 // 기본 라우트
 app.get("/", (req, res) => {
