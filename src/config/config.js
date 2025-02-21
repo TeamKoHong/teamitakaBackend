048dc2edb1375833be2e808bed535db047f648ee
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-21 11:50:34 +0900

    🔧 Sync config.js with Secrets
    Sync config.js with Secrets

--- src/config/config.js
+++ src/config/config.js
@@ -11,15 +11,15 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER || "iam_user",
-    password: process.env.DB_PASSWORD || "Teamitaka123!",
-    database: process.env.DB_NAME || "teamitaka_database",
-    host: process.env.DB_HOST || "35.223.147.232",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     dialectOptions: {
       ssl: false
     },
-    logging: console.log,  // 디버깅용 로그 활성화
+    logging: console.log,
   },
 };
