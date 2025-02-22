5d64ed0bf5a70f2b64999975533befa665d198ba
yewon <142921713+yewon4284@users.noreply.github.com>
2025-02-22 19:31:18 +0900

    3000->3001

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./src/app");  // Express 앱
 // 여기서 db.js가 필요하다면 import (단, 일반적으로 db.js는 다른 곳에서 import되며,
 // NODE_ENV !== 'test' 조건 하에 connectDB()가 이미 실행될 것)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
