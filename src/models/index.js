f926f424fb9719de7abb962f902d37a925f0a9fa
yewon <142921713+yewon4284@users.noreply.github.com>
2025-02-28 22:39:51 +0900

    ㅇ

--- src/models/index.js
+++ src/models/index.js
@@ -8,27 +8,28 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // ✅ 모델 불러오기
-db.User = require("./User")(sequelize, Sequelize.DataTypes);
+db.Admin = require("./Admin")(sequelize, Sequelize.DataTypes);
+db.Application = require("./Application")(sequelize, Sequelize.DataTypes);
+db.Campus = require("./Campus")(sequelize, Sequelize.DataTypes);
+db.College = require("./College")(sequelize, Sequelize.DataTypes);
+db.Comment = require("./Comment")(sequelize, Sequelize.DataTypes);
+db.Department = require("./Department")(sequelize, Sequelize.DataTypes);
+db.Hashtag = require("./Hashtag")(sequelize, Sequelize.DataTypes);
+db.Keyword = require("./Keyword")(sequelize, Sequelize.DataTypes);
+db.Like = require("./Like")(sequelize, Sequelize.DataTypes);
+db.Notification = require("./Notification")(sequelize, Sequelize.DataTypes);
+db.Profile = require("./Profile")(sequelize, Sequelize.DataTypes);
 db.Project = require("./Project")(sequelize, Sequelize.DataTypes);
+db.ProjectMembers = require("./ProjectMembers")(sequelize, Sequelize.DataTypes);
 db.Recruitment = require("./Recruitment")(sequelize, Sequelize.DataTypes);
 db.Review = require("./Review")(sequelize, Sequelize.DataTypes);
-db.Search = require("./Search")(sequelize, Sequelize.DataTypes);
-db.VerifiedEmail = require("./VerifiedEmail")(sequelize, Sequelize.DataTypes);
-db.Admin = require("./Admin")(sequelize, Sequelize.DataTypes);
-db.University = require("./University")(sequelize, Sequelize.DataTypes);
-db.College = require("./College")(sequelize, Sequelize.DataTypes);
-db.Department = require("./Department")(sequelize, Sequelize.DataTypes);
-db.Application = require("./Application")(sequelize, Sequelize.DataTypes);
-db.Hashtag = require("./Hashtag")(sequelize, Sequelize.DataTypes);
-db.Todo = require("./Todo")(sequelize, Sequelize.DataTypes);
-db.Timeline = require("./Timeline")(sequelize, Sequelize.DataTypes);
-db.Keyword = require("./Keyword")(sequelize, Sequelize.DataTypes);
-db.Notification = require("./Notification")(sequelize, Sequelize.DataTypes);
-db.Comment = require("./Comment")(sequelize, Sequelize.DataTypes);
 db.Scrap = require("./Scrap")(sequelize, Sequelize.DataTypes);
-db.ProjectMembers = require("./ProjectMembers")(sequelize, Sequelize.DataTypes);
-db.Campus = require("./Campus")(sequelize, Sequelize.DataTypes);
-db.Like = require("./Like")(sequelize, Sequelize.DataTypes);
+db.Search = require("./Search")(sequelize, Sequelize.DataTypes);
+db.Timeline = require("./Timeline")(sequelize, Sequelize.DataTypes);
+db.Todo = require("./Todo")(sequelize, Sequelize.DataTypes);
+db.University = require("./University")(sequelize, Sequelize.DataTypes);
+db.User = require("./User")(sequelize, Sequelize.DataTypes);
+db.VerifiedEmail = require("./VerifiedEmail")(sequelize, Sequelize.DataTypes);
 
 // 모델 간 관계 설정
 db.Application.associate = (models) => {
