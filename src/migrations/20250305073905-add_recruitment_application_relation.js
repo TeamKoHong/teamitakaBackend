e0d80c80ba2f8d803dd30c6251a4c9640252e156
yewon <142921713+yewon4284@users.noreply.github.com>
2025-03-05 16:47:22 +0900

    2

--- src/migrations/20250305073905-add_recruitment_application_relation.js
+++ src/migrations/20250305073905-add_recruitment_application_relation.js
@@ -2,18 +2,28 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("Applications", "recruitment_id", {
-      type: Sequelize.UUID,
-      allowNull: false,
-      references: {
-        model: "Recruitments",
-        key: "recruitment_id",
-      },
-      onDelete: "CASCADE",
-    });
+    // 테이블 구조 확인
+    const tableDefinition = await queryInterface.describeTable("Applications");
+
+    // recruitment_id 컬럼이 없을 경우에만 추가
+    if (!tableDefinition.recruitment_id) {
+      await queryInterface.addColumn("Applications", "recruitment_id", {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "Recruitments",
+          key: "recruitment_id",
+        },
+        onDelete: "CASCADE",
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("Applications", "recruitment_id");
+    // rollback 시에는 컬럼을 삭제
+    const tableDefinition = await queryInterface.describeTable("Applications");
+    if (tableDefinition.recruitment_id) {
+      await queryInterface.removeColumn("Applications", "recruitment_id");
+    }
   },
 };
