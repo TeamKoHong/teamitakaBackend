a3b2463db60114373ce24495183ea208f385549b
yewon <142921713+yewon4284@users.noreply.github.com>
2025-03-14 18:18:08 +0900

    공유일정2

---
+++ src/models/Schedule.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Schedule = sequelize.define(
+    "Schedule",
+    {
+      schedule_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      project_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: "Projects",
+          key: "project_id",
+        },
+        onDelete: "CASCADE",
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Schedules",
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+
+  return Schedule;
+};
