196aff456112f537332817ea9e3d37cb0aae1f59
_woo_s.j <91654658+sjwoo1999@users.noreply.github.com>
2025-12-04 14:31:59 +0900

    Woo (#247)
    * 🔨 Modified: README.md
    
    Modified: README.md
    
    * 🔨 Modified: scrapService.js
    
    Modified: scrapService.js

--- src/services/scrapService.js
+++ src/services/scrapService.js
@@ -1,12 +1,27 @@
-const { Scrap, Recruitment } = require("../models");
+const { Scrap, Recruitment, sequelize } = require("../models");
 
 const getUserScraps = async (user_id) => {
   return await Scrap.findAll({
     where: { user_id },
-    raw: true,
-    attributes: ["Recruitment.user_id", "Recruitment.title", "Recruitment.content", "Recruitment.scrap_count"],
-    include: [{ model: Recruitment, attributes: [] }],
-    order: [[Recruitment, "scrap_count", "desc"]],
+    include: [{
+      model: Recruitment,
+      attributes: [
+        'recruitment_id',
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'photo_url',
+        [
+          sequelize.literal(`(
+            SELECT COUNT(*) FROM scraps
+            WHERE scraps.recruitment_id = "Recruitment".recruitment_id
+          )`),
+          'scrap_count'
+        ]
+      ]
+    }],
+    order: [['createdAt', 'DESC']],
   });
 };
 
@@ -17,11 +32,9 @@ const toggleScrap = async (user_id, recruitment_id) => {
 
   if (!existScrap) {
     await Scrap.create({ user_id, recruitment_id });
-    await Recruitment.increment({ scrap_count: 1 }, { where: { recruitment_id } });
     return "스크랩 추가";
   } else {
     await Scrap.destroy({ where: { user_id, recruitment_id } });
-    await Recruitment.decrement({ scrap_count: 1 }, { where: { recruitment_id } });
     return "스크랩 취소";
   }
 };
